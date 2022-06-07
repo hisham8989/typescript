f7ffcd3fbc886355eed6b1faa308e4cf9eb002c2
hisham8989 <khanhisham25@gmail.com>
2022-06-07 14:58:29 +0530

    define tuple

---
+++ tupple.js
@@ -0,0 +1,5 @@
+// let role: string[] | number[] = ['admin', 'manager']
+// let role:[string,string,number,number] = ['admin','manager',1,5]
+var role = ['admin', 'manager', 1, 5];
+role.push(false);
+console.log(role);
---
+++ tupple.ts
@@ -0,0 +1,6 @@
+// let role: string[] | number[] = ['admin', 'manager']
+
+// let role:[string,string,number,number] = ['admin','manager',1,5]
+let role:[string,string,number,number,boolean?] = ['admin','manager',1,5]
+role.push(false)
+console.log(role);
